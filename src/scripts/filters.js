9e9659fa3698892d38f6fd337da0a555e423422d
olga210600 <89257451+olga210600@users.noreply.github.com>
2021-12-03 02:37:43 +0200

    added firat filter

--- src/scripts/filters.js
+++ src/scripts/filters.js
@@ -1,7 +1,7 @@
-
-
-
 const movies = [];
+// new
+const filteredFilms = []
+//
 const htmlElems = []; // добавить в HTML контейнер для вывода //фильмов 5 штук на экран
 const requestURLMovie = 'https://wowmeup.pp.ua/movie?page=1';// (variable for fetch request Movie) = https://wowmeup.pp.ua/movie
 const BtnLeft = document.querySelector('.BtnLeft');
@@ -15,7 +15,11 @@ const movieElement = document.createElement('img'); // создаем тег img
 function getMovies() {
     return fetch(requestURLMovie)
         .then(response => response.json())
-        .then((data)  => data.movies.forEach((obj) => movies.push(obj)))
+        .then((data)  => {
+            console.log('data: ', data)
+
+            data.movies.forEach((obj) => movies.push(obj))
+        })
         .catch(error => console.log(error))
 }
 getMovies().then (() => console.log(movies))
@@ -36,14 +40,13 @@ const films = document.querySelectorAll('.films')
 const naw = document.querySelector('nav');
 naw.addEventListener('click', (event )=> {
     if (event.target.tagName !== 'LI') return false;
-
     let filterClass = event.target.dataset['f'];
     console.log(filterClass)
-    movies.forEach((element) =>{
-        if(element.genre_ids[0] === films.values() ){
-            console.log('4242')
-        }else {
-            console.log('arr')
+    movies.forEach((element) => {
+        if (element.genre_ids.includes(+event.target.id)) {
+
+            filteredFilms.push(element)
         }
     })
+    console.log('filteredFilm', filteredFilms)
 })
