4bc8dd4d663f946339f60473442a163f21d5622c
olga210600 <89257451+olga210600@users.noreply.github.com>
2021-12-03 14:05:19 +0200

    added filters 2

--- src/scripts/filters.js
+++ src/scripts/filters.js
@@ -1,6 +1,10 @@
 const movies = [];
 // new
 const filteredFilms = []
+const adultFilms = []
+const budgetFilms = []
+
+
 //
 const htmlElems = []; // добавить в HTML контейнер для вывода //фильмов 5 штук на экран
 const requestURLMovie = 'https://wowmeup.pp.ua/movie?page=1';// (variable for fetch request Movie) = https://wowmeup.pp.ua/movie
@@ -15,14 +19,15 @@ const movieElement = document.createElement('img'); // создаем тег img
 function getMovies() {
     return fetch(requestURLMovie)
         .then(response => response.json())
-        .then((data)  => {
+        .then((data) => {
             console.log('data: ', data)
 
             data.movies.forEach((obj) => movies.push(obj))
         })
         .catch(error => console.log(error))
 }
-getMovies().then (() => console.log(movies))
+
+getMovies().then(() => console.log(movies))
 
 function creatMovie(item) {// вывели на страничку пять фильмов
     if (!movies) {
@@ -33,20 +38,66 @@ function creatMovie(item) {// вывели на страничку пять фи
         htmlElems.push(movieElement);
     }
 }
+
 creatMovie() // вывели на страничку пять фильмов
 
 
 const films = document.querySelectorAll('.films')
 const naw = document.querySelector('nav');
-naw.addEventListener('click', (event )=> {
+const adult = document.querySelector('.adult');
+const btnFilter = document.querySelector('.btnFilter');
+const minNumberBudget = document.querySelector('.minNumberBudget');
+const maxNumberBudget = document.querySelector('.maxNumberBudget');
+
+
+naw.addEventListener('click', (event) => {
     if (event.target.tagName !== 'LI') return false;
     let filterClass = event.target.dataset['f'];
+    // li.style.color = 'red'
     console.log(filterClass)
     movies.forEach((element) => {
-        if (element.genre_ids.includes(+event.target.id)) {
 
+        if (element.genre_ids.includes(+event.target.id)) {
             filteredFilms.push(element)
         }
     })
     console.log('filteredFilm', filteredFilms)
+})
+
+adult.addEventListener('click', (event) => {
+    filteredFilms.forEach((item) => {
+        if (item.adult === true) {
+            adultFilms.push(item)
+            console.log(adultFilms)
+        }
+    })
+    console.log('adultFilm', adultFilms)
+})
+
+
+
+
+let minBudget = minNumberBudget.value;
+let maxBudget = maxNumberBudget.value;
+
+
+const getBudget = (min, max) => {
+    filteredFilms.forEach((el) => {
+        if (el.budget >= min && el.budget <= max ){
+            budgetFilms.push(el)
+
+        }
+    })
+}
+
+
+btnFilter.addEventListener('click', (el) => {
+    // debugger
+    // console.log()
+    console.log(maxBudget)
+    console.log(minBudget)
+    getBudget()
+
+
+    console.log(budgetFilms)
 })
